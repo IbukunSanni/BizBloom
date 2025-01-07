d84f597715cce10d1b0780b26c404773347a99fa
IbukunSanni <ibukun.sanni@gmail.com>
2025-01-07 17:13:30 -0500

    Backend updated

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -28,6 +28,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# TODO: check is false in production
 DEBUG = env("DEBUG")
 
 # TODO: update ALLOWED_HOSTS to have details for Amazon EC2
@@ -65,6 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite dev server
     "http://127.0.0.1:5173",
+    "https://bizbloom.vercel.app",
 ]
 
 ROOT_URLCONF = "app.urls"
